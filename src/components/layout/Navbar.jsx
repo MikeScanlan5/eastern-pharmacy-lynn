271544f4229ba9053bae63e1b29af346ab4eae87
Mike Scanlan <mike@bstn.io>
2026-04-21 08:09:55 +0000

    File changes

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -33,7 +33,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 right-0 z-50 bg-[#0F172A] border-b border-white/10">
+      <nav className="fixed top-0 left-0 right-0 z-50 border-b border-white/10" style={{ backgroundColor: '#0F172A' }}>
         <div className="max-w-7xl mx-auto px-6 lg:px-8">
           <div className="flex items-center justify-between h-20">
             {/* Logo */}
